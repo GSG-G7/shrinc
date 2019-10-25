3713107da4f008ea4d0a86cc7fbd406475b0ffb4
fadeomar <fadeomar2015@gmail.com>
2019-10-25 17:42:44 +0300

    handle the static and the request from the front and the urlencoded

--- server/app.js
+++ server/app.js
@@ -1,4 +1,6 @@
+/* eslint-disable no-unused-vars */
 const express = require('express');
+const { join } = require('path');
 require('dotenv').config();
 
 const router = require('./controller');
@@ -7,7 +9,16 @@ const app = express();
 app.use(express.json());
 app.set('port', process.env.PORT || 5000);
 
-app.use(express.json());
-app.get('*', router);
+app.use('/api/v1/', router);
 
+app.use(express.static(join(__dirname, '..', 'client', 'public')));
+
+app.get('*', (_req, res) => {
+  res.sendFile(join(__dirname, '..', 'client', 'public', 'index.html'));
+});
+
+app.use((err, req, res, next) => {
+  console.log(req.path, err);
+  res.status(500).send({ message: 'Internal Server Error', statusCode: 500 });
+});
 module.exports = app;
--- server/index.js
+++ server/index.js
@@ -2,5 +2,5 @@ const app = require('./app');
 
 const port = app.get('port');
 app.listen(port, () => {
-  console.log(`App is on http://localhost:${port}`);
+  console.log(`server is running at http://localhost:${port}`);
 });
