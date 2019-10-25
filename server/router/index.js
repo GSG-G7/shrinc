cd780bda46825f10676237352d4f39e70f29c071
asmaa <asmathabet2009@hotmail.com>
2019-10-25 17:41:56 +0300

    handel client & server errors

--- server/router/index.js
+++ server/router/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 // call controllers modules here
+const { clientError, serverError } = require('../controllers');
 
 const router = express.Router();
 // write down your routes
+
+router.use(clientError);
+router.use(serverError);
 
 module.exports = router;
