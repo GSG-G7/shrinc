4931e38af9d6b5f46d40070965085d95cf716693
fadeomar <fadeomar2015@gmail.com>
2019-10-26 23:19:04 +0300

    create a button instaide of div for menu icon

--- client/src/components/common/Header/index.js
+++ client/src/components/common/Header/index.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { MenuIcon } from '../../assets';
+import { MenuIcon, headerLogo } from '../../assets';
 import './style.css';
 
 class Header extends Component {
   state = {};
 
+  onclick = () => {
+    console.log('fadi asd');
+  };
+
   render() {
     return (
       <div className="header">
         <div className="header__bar">
-          <div className="menu-icon"></div>
-          <Link to="fade">
-            <MenuIcon />
+          <button type="button" className="menu-icon" onClick={this.onclick}>
+            <MenuIcon onClick={this.onclick} />
+          </button>
+          <Link to="home">
+            <img src={headerLogo} alt="headerLogo" />
           </Link>
         </div>
       </div>
