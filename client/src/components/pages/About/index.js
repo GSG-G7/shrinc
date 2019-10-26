a11e338d395c51968f099dcf90da3ac053a4c189
hashemt <hashemta77@gmail.com>
2019-10-26 13:54:35 +0200

    create About Page file Relates #43

---
+++ client/src/components/pages/About/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section>
+      <h2>About us</h2>
+      <p>
+        Shrinc is the new way to access talking therapies, quickly and easily
+      </p>
+    </section>
+  );
+};
+
+export default About;
