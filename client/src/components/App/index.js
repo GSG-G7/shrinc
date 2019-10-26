9eaf16c2c136f132c710773f9a4d1dd63a6f868b
fadeomar <fadeomar2015@gmail.com>
2019-10-27 00:58:24 +0300

    delete tsting code from app file

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,18 +1,8 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Header from '../common/Header';
 import './style.css';
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" component={Header} />
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
