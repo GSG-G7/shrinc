6dd1c8efff8415b511b45594b3c015cf20a8b435
fadeomar <fadeomar2015@gmail.com>
2019-10-26 22:25:43 +0300

    rename the header file to index

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import Header from '../common/Header';
 import './style.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/common/Header/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  state = {};
+
+  render() {
+    return <div>fadi asd</div>;
+  }
+}
+
+export default Header;
