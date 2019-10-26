f84d81cb2c416cbe6b53a1e9d6cf445d7cf9fc73
fadeomar <fadeomar2015@gmail.com>
2019-10-27 00:52:46 +0300

    menu in click opend and closed

--- client/src/components/assets/index.js
+++ client/src/components/assets/index.js
@@ -1,5 +1,19 @@
 import headerLogo from './menuTopLogo.svg';
 import MenuIcon from './MenuIcon';
 import MenuLogo from './MenuLogo.svg';
+import HomeIcone from './HomeIcone';
+import TypesTherapist from './TypesTherapist';
+import Questionnaire from './Questionnaire';
+import Search from './Search';
+import About from './About';
 
-export { MenuIcon, headerLogo, MenuLogo };
+export {
+  MenuIcon,
+  headerLogo,
+  MenuLogo,
+  HomeIcone,
+  TypesTherapist,
+  Questionnaire,
+  Search,
+  About,
+};
--- client/src/components/common/Header/index.js
+++ client/src/components/common/Header/index.js
@@ -1,16 +1,29 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { MenuIcon, headerLogo, MenuLogo } from '../../assets';
+import menuData from './menuData';
 import './style.css';
 
 class Header extends Component {
-  state = {};
+  state = {
+    isClosed: true,
+  };
 
   onclick = () => {
-    console.log('fadi asd');
+    const { isClosed } = this.state;
+    this.setState({ isClosed: !isClosed });
+  };
+
+  onclickForModel = e => {
+    if (e.target.id === 'model') {
+      this.setState({ isClosed: true });
+    }
   };
 
   render() {
+    const { isClosed } = this.state;
+
     return (
       <div className="header">
         <div className="header__bar">
@@ -18,19 +31,32 @@ class Header extends Component {
             <MenuIcon onClick={this.onclick} />
           </button>
           <Link to="home">
-            <img src={headerLogo} alt="headerLogo" />
+            <img src={headerLogo} alt="header logo" />
           </Link>
         </div>
         {/* side bar */}
-        <div className="menu-model" id="model">
-          <nav className="nav">
-            <div className="menu__head">
+
+        <div
+          className={!isClosed ? 'menu__model' : ''}
+          onClick={this.onclickForModel}
+          id="model"
+        >
+          <nav className={isClosed ? 'hide' : 'menu__nav'}>
+            <div className="menu__head" onClick={this.onclick}>
               <h2 className="menu__title">Menu</h2>
             </div>
             <ul className="menu__list">
               <div className="menu_img">
                 <img src={MenuLogo} alt="shrinc" />
               </div>
+              {menuData.map(item => (
+                <li key={item.label} className="menu__item">
+                  {item.icon ? item.icon : ''}
+                  <Link to={item.url} className="menu__link">
+                    {item.label}
+                  </Link>
+                </li>
+              ))}
             </ul>
           </nav>
         </div>
