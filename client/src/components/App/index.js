d1d09768c369acec42cb6bdf35c268b828915fee
fadeomar <fadeomar2015@gmail.com>
2019-10-27 14:20:32 +0200

    delete un used style from css file

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -3,8 +3,19 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import './style.css';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Header from '../common/Header';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={Header} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
