3bf61dfae446a70938493d5bd814998ffe5169bd
asmaa <asmathabet2009@hotmail.com>
2019-10-27 21:18:42 +0200

    inital prepare for Home page relates #28

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Home from '../pages/Home';
+import 'antd/dist/antd.css';
 
 import './style.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/pages/Home/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Icon from 'antd/lib/icon';
+import Button from '../../common/Button';
+import shrincHome from '../../../assets/shrincHome.svg';
+import './style.css';
+
+const HomePage = () => {
+  return (
+    <div className="home">
+      <div className="home__header">
+        <div className="home__image">
+          <img src={shrincHome} alt="class-img" className="vision__img"></img>
+        </div>
+        <div className="home__box">
+          <h3 className="home__title">Shrinc</h3>
+          <h5 className="home__subTitle">therapy, simplified</h5>
+        </div>
+      </div>
+      <div className="home__description">
+        The key to natures therapy is feeling like a tiny part of it, not a
+        master over it.
+      </div>
+      <div className="home__actions">
+        <div className="home_buttons">
+          <Link to="/types" className="home__buttons__types">
+            <Icon type="user" />
+            <Button
+              value="Find the Right Therapy for You"
+              className="home__buttons__types-btn"
+            />
+          </Link>
+          <Link to="/filter" className="home__buttons__filter">
+            <Icon type="user" />
+            <Button
+              value="Search for Therapists"
+              className="home__buttons__filter-btn"
+            />
+          </Link>
+        </div>
+        <div className="home__signup">
+          <p>Are you therapist?</p>
+          <span>
+            <Link to="/signup" className="home__signup">
+              sign up
+            </Link>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
