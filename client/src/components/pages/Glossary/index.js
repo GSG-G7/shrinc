301ce828772979453f9a98444ad4824f7f780620
MohammadAlhalaq <m-a-s-h@outlook.com>
2019-10-27 09:15:25 +0200

    delete header Relates #46

--- client/src/components/pages/Glossary/index.js
+++ client/src/components/pages/Glossary/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Header from './header';
 import { Title, Description, Table } from './style';
 import table from '../../../assets/table.svg';
 
 function glossary() {
   return (
     <div>
-      <Header />
       <Title>Glossary</Title>
       <Description>You can find Glossary of Therap here to know .</Description>
       <Table src={table} alt="table Image" />
