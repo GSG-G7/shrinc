4488b18b30581fdce3be1e9dd291fa5a234fccfd
hashemt <hashemta77@gmail.com>
2019-10-27 09:15:41 +0200

    edit class names Relates #43

--- client/src/components/pages/About/index.js
+++ client/src/components/pages/About/index.js
@@ -4,19 +4,19 @@ import './style.css';
 
 const About = () => {
   return (
-    <section className="page">
-      <h2 className="page-header">About us</h2>
+    <section className="about-page">
+      <h2 className="about-page__title">About us</h2>
       <ul>
-        <li className="page-point">
+        <li className="about-page__point">
           Our Mission
-          <p className="page-parg">
+          <p className="about-page__content">
             Shrinc is the new way to access talking therapies, quickly and
             easily
           </p>
         </li>
-        <li className="page-point">
+        <li className="about-page__point">
           Our Vision
-          <p className="page-parg">
+          <p className="about-page__content">
             Shrinc provides users with free access to a complete overview of the
             main forms of talking therapy, which are presented in a clear and
             concise manner. Shrinc also contains a database of therapists that
@@ -24,18 +24,18 @@ const About = () => {
             availability, and the option of remote consultation.
           </p>
         </li>
-        <li className="page-point">
+        <li className="about-page__point">
           Questionnire
-          <p className="page-parg">
+          <p className="about-page__content">
             Users can also complete a questionnaire to help identify the type of
             therapy that is most suitable for them. Shrinc will then present the
             most relevant therapists, allowing the user to apply further filters
             as described above.
           </p>
         </li>
-        <li className="page-point">
+        <li className="about-page__point">
           Review system
-          <p className="page-parg">
+          <p className="about-page__content">
             All therapists are verified for their qualifications and
             professional accreditation. There is a review system that allows
             Shrinc customers to leave feedback of their experience. All reviews
@@ -45,17 +45,17 @@ const About = () => {
             from the database if necessary.
           </p>
         </li>
-        <li className="page-point">
+        <li className="about-page__point">
           Select therapist
-          <p className="page-parg">
+          <p className="about-page__content">
             Once a user has selected a therapist, users can register with Shrinc
             for free which allows them to easily book a consultation using a
             web-based calendar.
           </p>
         </li>
-        <li className="page-point">
+        <li className="about-page__point">
           Payment
-          <p className="page-parg">
+          <p className="about-page__content">
             Payment is made through Shrinc using either Paypal or debit card at
             the rate provided by the therapist.
           </p>
