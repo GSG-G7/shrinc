7fdf538162f45fd4c35ea1b6b5925ffac727750e
hashemt <hashemta77@gmail.com>
2019-10-27 15:03:40 +0200

    add part of validation Relates #48

--- client/src/components/pages/Signup/index.js
+++ client/src/components/pages/Signup/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import {
   Form,
   Input,
@@ -31,6 +32,10 @@ class Signup extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
+    const {
+      form: { validateFieldsAndScroll },
+    } = this.props;
+    validateFieldsAndScroll();
   };
 
   handleChange = ({ target: input }) => {
@@ -87,26 +92,76 @@ class Signup extends Component {
 
   render() {
     const { Option } = Select;
+    const {
+      therapistInfo: { remote },
+    } = this.state;
+    const {
+      form: { getFieldDecorator },
+    } = this.props;
+    // console.log(this.props);
     return (
       <div>
         <h2>Therapist Signup</h2>
         <Form onSubmit={this.handleSubmit}>
           <Form.Item label="Full Name:">
-            <Input
-              name="fullName"
-              onChange={this.handleChange}
-              placeholder=""
-            />
+            {getFieldDecorator('fullName', {
+              rules: [
+                {
+                  message: 'The input is not valid!',
+                },
+                {
+                  required: true,
+                  message: 'Please input your full name!',
+                },
+                {
+                  min: 7,
+                  message: 'must be at least 7 character',
+                },
+              ],
+            })(
+              <Input
+                name="fullName"
+                onChange={this.handleChange}
+                placeholder="Enter your full name"
+              />
+            )}
           </Form.Item>
           <Form.Item label="Email:">
-            <Input name="email" onChange={this.handleChange} placeholder="" />
+            {getFieldDecorator('email', {
+              rules: [
+                {
+                  type: 'email',
+                  message: 'The input is not valid E-mail!',
+                },
+                {
+                  required: true,
+                  message: 'Please input your E-mail!',
+                },
+              ],
+            })(
+              <Input
+                onChange={this.handleChange}
+                placeholder="Enter your email"
+              />
+            )}
           </Form.Item>
           <Form.Item label="Password:">
-            <Input.Password
+            {getFieldDecorator('password', {
+              rules: [
+                {
+                  required: true,
+                  message: 'Please input your password!',
+                },
+                {
+                  validator: this.validateToNextPassword,
+                },
+              ],
+            })(<Input.Password />)}
+            {/* <Input.Password
               name="password"
               onChange={this.handleChange}
               placeholder=""
-            />
+            /> */}
           </Form.Item>
           <Form.Item label="Confirm Password:">
             <Input.Password
@@ -118,20 +173,38 @@ class Signup extends Component {
             <Input name="city" onChange={this.handleChange} placeholder="" />
           </Form.Item>
           <Form.Item label="Type Of Therapy:">
-            <Select placeholder="Select a type" onChange={this.handleType}>
-              <Option value="CBT">Cognitive behavioural therapy</Option>
-              <Option value="PD">Psyhcodynamic therapy</Option>
-              <Option value="Hu">Humanistic psychotherapy</Option>
-              <Option value="In">Integrative psychotherapy</Option>
-            </Select>
+            {getFieldDecorator('types', {
+              rules: [
+                {
+                  required: true,
+                  message: 'Please select type!',
+                },
+              ],
+            })(
+              <Select placeholder="Select a type" onChange={this.handleType}>
+                <Option value="CBT">Cognitive behavioural therapy</Option>
+                <Option value="PD">Psyhcodynamic therapy</Option>
+                <Option value="Hu">Humanistic psychotherapy</Option>
+                <Option value="In">Integrative psychotherapy</Option>
+              </Select>
+            )}
           </Form.Item>
           <Form.Item label="Price Range:">
-            <Select placeholder="Select a range" onChange={this.handlePrice}>
-              <Option value="20-30">20-30</Option>
-              <Option value="30-40">30-40</Option>
-              <Option value="40-50">40-50</Option>
-              <Option value="50-60">50-60</Option>
-            </Select>
+            {getFieldDecorator('priceRange', {
+              rules: [
+                {
+                  required: true,
+                  message: 'Please select price range!',
+                },
+              ],
+            })(
+              <Select placeholder="Select a range" onChange={this.handlePrice}>
+                <Option value="20-30">20-30</Option>
+                <Option value="30-40">30-40</Option>
+                <Option value="40-50">40-50</Option>
+                <Option value="50-60">50-60</Option>
+              </Select>
+            )}
           </Form.Item>
           <Form.Item label="Post Code:">
             <Input
@@ -156,9 +229,13 @@ class Signup extends Component {
           <Form.Item label="Remote Therapy:">
             <Switch name="remote" onChange={this.handleRemote} />
           </Form.Item>
-          <Form.Item label="Skype:">
-            <Input name="skype" onChange={this.handleChange} placeholder="" />
-          </Form.Item>
+          {remote ? (
+            <Form.Item label="Skype:">
+              <Input name="skype" onChange={this.handleChange} placeholder="" />
+            </Form.Item>
+          ) : (
+            ''
+          )}
           <Form.Item>
             <Checkbox onChange={this.handleChecked}>
               I Accept the Terms of Services
@@ -173,4 +250,13 @@ class Signup extends Component {
   }
 }
 
-export default Signup;
+Signup.propTypes = {
+  form: PropTypes.objectOf(PropTypes.func).isRequired,
+  getFieldDecorator: PropTypes.func.isRequired,
+};
+
+const WrappedTimeRelatedForm = Form.create({ name: 'time_related_controls' })(
+  Signup
+);
+
+export default WrappedTimeRelatedForm;
