dcc9f961257f8c8e40bc15697de241152534734b
fadeomar <fadeomar2015@gmail.com>
2019-10-27 13:43:23 +0200

    edit the style

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 import 'antd/dist/antd.css';
 import './style.css';
+import TabelAvailability from '../common/availabilityTable';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <TabelAvailability></TabelAvailability>
+    </div>
+  );
 }
 
 export default App;
