ee0652ad6234dc7bbeba03c57a9a8a78601f439b
MohammadAlhalaq <m-a-s-h@outlook.com>
2019-10-27 09:33:09 +0200

    final editing Relates #46

--- client/src/components/pages/Glossary/index.js
+++ client/src/components/pages/Glossary/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Title, Description, Table } from './style';
+import './style.css';
 import table from '../../../assets/table.svg';
 
 function glossary() {
   return (
     <div>
-      <Title>Glossary</Title>
-      <Description>You can find Glossary of Therap here to know .</Description>
-      <Table src={table} alt="table Image" />
+      <h1>Glossary</h1>
+      <p>You can find Glossary of Therap here to know .</p>
+      <img src={table} alt="table" />
     </div>
   );
 }
