56da0145991bb84c03db8af0a997d8470059472f
fadeomar <fadeomar2015@gmail.com>
2019-10-27 13:46:53 +0200

    rreset the app file

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -2,14 +2,9 @@ import React from 'react';
 
 import 'antd/dist/antd.css';
 import './style.css';
-import TabelAvailability from '../common/availabilityTable';
 
 function App() {
-  return (
-    <div className="App">
-      <TabelAvailability></TabelAvailability>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
