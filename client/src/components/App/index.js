2440e8b956cd382bd9e00a77794e05db9bbf88b0
fadeomar <fadeomar2015@gmail.com>
2019-10-27 10:20:55 +0200

    install ant design and moment packages

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import TabelAvailability from '../common/availabilityTable';
 import './style.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <TabelAvailability />
+    </div>
+  );
 }
 
 export default App;
--- client/src/components/common/availabilityTable/index.js
+++ client/src/components/common/availabilityTable/index.js
@@ -1,1 +1,159 @@
+import React from 'react';
+import 'antd/dist/antd.css';
 import './style.css';
+import { Table, TimePicker } from 'antd';
+import moment from 'moment';
+
+const TabelAvailability = () => {
+  const columns = [
+    {
+      title: 'Day',
+      dataIndex: 'day',
+      key: 'day',
+      render: text => <span>{text}</span>,
+    },
+    {
+      title: 'From',
+      dataIndex: 'from',
+      key: 'from',
+    },
+    {
+      title: 'To',
+      dataIndex: 'to',
+      key: 'to',
+    },
+  ];
+
+  const data = [
+    {
+      key: '0',
+      day: 'Mon.',
+      from: (
+        <TimePicker
+          use12Hours
+          format="h:mm a"
+          defaultOpenValue={moment('00:00 am', 'HH:mm A')}
+        />
+      ),
+      to: (
+        <TimePicker
+          use12Hours
+          format="h:mm a"
+          defaultOpenValue={moment('00:00 am', 'HH:mm A')}
+        />
+      ),
+    },
+    {
+      key: '1',
+      day: 'Tue.',
+      from: (
+        <TimePicker
+          use12Hours
+          format="h:mm a"
+          defaultOpenValue={moment('00:00 am', 'HH:mm A')}
+        />
+      ),
+      to: (
+        <TimePicker
+          use12Hours
+          format="h:mm a"
+          defaultOpenValue={moment('00:00 am', 'HH:mm A')}
+        />
+      ),
+    },
+    {
+      key: '2',
+      day: 'Wed.',
+      from: (
+        <TimePicker
+          use12Hours
+          format="h:mm a"
+          defaultOpenValue={moment('00:00 am', 'HH:mm A')}
+        />
+      ),
+      to: (
+        <TimePicker
+          use12Hours
+          format="h:mm a"
+          defaultOpenValue={moment('00:00 am', 'HH:mm A')}
+        />
+      ),
+    },
+    {
+      key: '3',
+      day: 'Thu.',
+      from: (
+        <TimePicker
+          use12Hours
+          format="h:mm a"
+          defaultOpenValue={moment('00:00 am', 'HH:mm A')}
+        />
+      ),
+      to: (
+        <TimePicker
+          use12Hours
+          format="h:mm a"
+          defaultOpenValue={moment('00:00 am', 'HH:mm A')}
+        />
+      ),
+    },
+    {
+      key: '4',
+      day: 'Fri.',
+      from: (
+        <TimePicker
+          use12Hours
+          format="h:mm a"
+          defaultOpenValue={moment('00:00 am', 'HH:mm A')}
+        />
+      ),
+      to: (
+        <TimePicker
+          use12Hours
+          format="h:mm a"
+          defaultOpenValue={moment('00:00 am', 'HH:mm A')}
+        />
+      ),
+    },
+    {
+      key: '5',
+      day: 'Sat.',
+      from: (
+        <TimePicker
+          use12Hours
+          format="h:mm a"
+          defaultOpenValue={moment('00:00 am', 'HH:mm A')}
+        />
+      ),
+      to: (
+        <TimePicker
+          use12Hours
+          format="h:mm a"
+          defaultOpenValue={moment('00:00 am', 'HH:mm A')}
+        />
+      ),
+    },
+    {
+      key: '6',
+      day: 'Sun.',
+      from: (
+        <TimePicker
+          use12Hours
+          format="h:mm a"
+          defaultOpenValue={moment('00:00 am', 'HH:mm A')}
+        />
+      ),
+      to: (
+        <TimePicker
+          use12Hours
+          format="h:mm a"
+          defaultOpenValue={moment('00:00 am', 'HH:mm A')}
+        />
+      ),
+    },
+  ];
+
+  return <Table columns={columns} dataSource={data} className="table" />;
+};
+
+export default TabelAvailability;
