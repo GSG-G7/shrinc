06e5e7ae46d57d808b90fd10de2b21f1468dc76b
asmaa <asmathabet2009@hotmail.com>
2019-10-27 22:53:46 +0200

    add BrowserRouter to app.js relates #28

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import Home from '../pages/Home';
 import 'antd/dist/antd.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Home />
-      </div>
+      <div className="App"></div>
     </BrowserRouter>
   );
 }
