7a4fc7356397bd45bc25d0f8f55215b004baec69
MohammadAlhalaq <m-a-s-h@outlook.com>
2019-10-27 10:21:01 +0200

    add class to element Relates #46

--- client/src/components/pages/Glossary/index.js
+++ client/src/components/pages/Glossary/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
+
 import './style.css';
 import table from '../../../assets/table.svg';
 
 function glossary() {
   return (
-    <div>
-      <h1>Glossary</h1>
-      <p>You can find Glossary of Therap here to know .</p>
-      <img src={table} alt="table" />
-    </div>
+    <section>
+      <h1 className="glossary_title">Glossary</h1>
+      <p className="glossary_description">
+        You can find Glossary of Therap here to know .
+      </p>
+      <img className="glossary_table" src={table} alt="table for information" />
+    </section>
   );
 }
 
