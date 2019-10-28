c4b1181355f7745081f2dd520f76a5514afd400f
fadeomar <fadeomar2015@gmail.com>
2019-10-28 11:14:55 +0200

    add style for filter component

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 import 'antd/dist/antd.css';
 import './style.css';
+import Filter from '../common/Filter';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Filter></Filter>
+    </div>
+  );
 }
 
 export default App;
--- client/src/components/common/Filter/index.js
+++ client/src/components/common/Filter/index.js
@@ -1,1 +1,105 @@
+import React, { Component } from 'react';
 import './style.css';
+import { Select, Switch, Icon } from 'antd';
+
+const { Option } = Select;
+
+class Filter extends Component {
+  state = {
+    typeOfTherapy: '',
+    location: '',
+    PriceRange: '',
+    isRemote: true,
+  };
+
+  citeies = [
+    'London',
+    ' Midlands',
+    ' Manchester',
+    'West-Yorkshire ',
+    'Merseyside',
+    'South-Yorkshire ',
+    'Tyne-&-Wea',
+  ];
+
+  types = [
+    'Interpersonal',
+    'Family-focused',
+    'Dialectical-behavior',
+    'Cognitive-behavioral',
+  ];
+
+  ranges = ['1000-2000', '2000-3000', '3000-4000', '4000<'];
+
+  handleChange = (key, asd) => {
+    this.setState(
+      () => ({ [asd.props.state]: key }),
+      () => console.log(this.state)
+    );
+  };
+
+  handleClick = e => this.setState(() => ({ isRemote: e }));
+
+  render() {
+    return (
+      <div>
+        <label htmlFor="business">
+          <span>Type of therapy</span>
+          <Select
+            key="typeOfTherapy"
+            defaultValue="selecte type"
+            style={{ width: 200, color: 'red' }}
+            onChange={this.handleChange}
+          >
+            {this.types.map(type => (
+              <Option value={type} state="typeOfTherapy" key={type}>
+                {type} Therapy
+              </Option>
+            ))}
+          </Select>
+        </label>
+        <label>
+          <span>Location</span>
+          <Select
+            key="location"
+            defaultValue="selecte location"
+            style={{ width: 200, color: 'red' }}
+            onChange={this.handleChange}
+          >
+            {this.citeies.map(city => (
+              <Option value={city} state="location" key={city}>
+                {city}
+              </Option>
+            ))}
+          </Select>
+        </label>
+        <label>
+          <span>Price range</span>
+          <Select
+            key="price-range"
+            defaultValue="selecte range"
+            style={{ width: 200, color: 'red' }}
+            onChange={this.handleChange}
+          >
+            {this.ranges.map(range => (
+              <Option value={range} state="PriceRange" key={range}>
+                {range}
+              </Option>
+            ))}
+          </Select>
+        </label>
+        <div>
+          <span>Remote therapy</span>
+          <Switch
+            checkedChildren={<Icon type="check" />}
+            unCheckedChildren={<Icon type="close" />}
+            defaultChecked
+            onClick={this.handleClick}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Filter;
