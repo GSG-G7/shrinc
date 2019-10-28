141469776fb2229b736a8460d81982ddb8c7057d
fadeomar <fadeomar2015@gmail.com>
2019-10-29 01:08:31 +0200

    add static data for test the componrnt

--- client/src/components/common/Card/staticDataTesting.js
+++ client/src/components/common/Card/staticDataTesting.js
@@ -7,6 +7,21 @@ const staticData = [
     approach: 'Therpay approach',
     type: 'Therpay Type',
   },
+  {
+    name: 'hashem',
+    priceRange: 8888,
+    avatar: 'https://connectbuncombe.org/wp-content/uploads/2019/05/woman.png',
+    approach: 'Therpay approach',
+    type: 'Therpay Type',
+  },
+  {
+    name: 'mohmed',
+    priceRange: 7777,
+    avatar:
+      'https://cdn.pixabay.com/photo/2014/04/03/10/07/faceless-309882_960_720.png',
+    approach: 'Therpay approach',
+    type: 'Therpay Type',
+  },
 ];
 
 export default staticData;
