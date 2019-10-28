b40e85eea1276ab6195fb68f0119fb63c21143d1
fadeomar <fadeomar2015@gmail.com>
2019-10-29 00:41:31 +0200

    editer the heder in the card compnent

--- client/src/components/common/Card/index.js
+++ client/src/components/common/Card/index.js
@@ -1,30 +1,26 @@
 import React from 'react';
-import { Collapse } from 'antd';
+import { Icon, Collapse } from 'antd';
 import './style.css';
 import CardContent from './cardContent';
 import data from './staticDataTesting';
 
 const { Panel } = Collapse;
 
-const text = `
-  A dog is a type of domesticated animal.
-  Known for its loyalty and faithfulness,
-  it can be found as a welcome guest in many households across the world.
-`;
-
-const Card = () => (
-  <Collapse accordion>
-    <Panel header="This is panel header 1" key="1">
-      {/* <p>asd</p> */}
-      <CardContent data={data} />
-    </Panel>
-    <Panel header="This is panel header 2" key="2">
-      <p>{text}</p>
-    </Panel>
-    <Panel header="This is panel header 3" key="3">
-      <p>{text}</p>
-    </Panel>
-  </Collapse>
-);
+const genExtra = price => <span>{price} $/Session</span>;
+const Card = () => {
+  return (
+    <Collapse accordion>
+      {data.map(therpy => (
+        <Panel
+          header={therpy.name}
+          key={therpy.name}
+          extra={genExtra(therpy.priceRange)}
+        >
+          <CardContent data={therpy} />
+        </Panel>
+      ))}
+    </Collapse>
+  );
+};
 
 export default Card;
