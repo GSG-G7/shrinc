ea765a207e8083ffed1b04d75a644ea9eb384411
hashemt <hashemta77@gmail.com>
2019-10-28 13:35:05 +0200

    create middleware signup function Relates #59

---
+++ server/controllers/routes/signup.js
@@ -0,0 +1,13 @@
+// const { hashPassowrd } = require('../utils/hashingPassword');
+// const signupSchema = require('../validation/signupValidation');
+
+// module.exports = async (req, res) => {
+//   const therapist = req.body;
+//   console.log(req.body);
+//   try {
+//     await signupSchema.validate(therapist);
+//     therapist.password = await hashPassowrd(therapist.password);
+//   } catch (err) {
+
+//   }
+// };
