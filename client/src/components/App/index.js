b6e927861624dcf0591066fa83dbd0a297d79ed6
fadeomar <fadeomar2015@gmail.com>
2019-10-28 13:45:12 +0200

    create card component file

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 import 'antd/dist/antd.css';
 import './style.css';
+import Card from '../common/Card';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Card />
+    </div>
+  );
 }
 
 export default App;
