85d19af675e13a2d8c84777038aeadb3c2deb3ea
fadeomar <fadeomar2015@gmail.com>
2019-10-28 14:19:37 +0200

    reset the app file

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import Filter from '../common/Filter';
 
 import 'antd/dist/antd.css';
 import './style.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Filter></Filter>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
