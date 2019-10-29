dc6e4b6a6d54c0d59e05065c5fd6ca0308c70eae
asmaa <asmathabet2009@hotmail.com>
2019-10-29 11:29:21 +0200

    create Questionnare Result page relates #41

--- client/src/components/pages/Results/index.js
+++ client/src/components/pages/Results/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import Data from './Data';
+import Card from '../../common/Card';
+
 import './style.css';
 
-const ResultsPage = () => {
+const ResultsPage = Data => {
   return (
     <div className="Results">
       <div className="Results__TherapyType">
         <h3 className="Results__TherapyType__title">Therapy Type</h3>
-        <h4>{Data}</h4>
+        {Data.map(dataType => (
+          <h4 className="Results__TherapyType__name">{dataType.type}</h4>
+        ))}
       </div>
-      <div className="Results__TherapistsNames"></div>
+      <div className="Results__TherapistsNames">
+        <h3 className="Results__TherapistsNames_suggest">
+          According to your answers, we would suggest that you search for
+          therapists specializing in:
+        </h3>
+        <div className="Results__TherapistsNames__Cards">
+          <Card />
+        </div>
+      </div>
     </div>
   );
 };
