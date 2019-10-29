a06e4cc2511a66b03ca1b7f56924761157f8a138
MohammadAlhalaq <m-a-s-h@outlook.com>
2019-10-29 13:56:26 +0200

    remove pacage json

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -3,8 +3,15 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import './style.css';
 
+import Profile from '../pages/Profile';
+import Data from '../pages/Profile/profileData';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Profile profileData={Data} />
+    </div>
+  );
 }
 
 export default App;
