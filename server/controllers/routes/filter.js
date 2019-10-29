7a988ce2d16c872100fb3b707d5e8cff84b3af79
hashemt <hashemta77@gmail.com>
2019-10-29 13:04:33 +0200

    refactor function Relates #58

--- server/controllers/routes/filter.js
+++ server/controllers/routes/filter.js
@@ -1,8 +1,13 @@
+const base = require('../../database/config/airtableConnection');
 const { getFilteredTherapist } = require('../utils/filerFunction');
 
 module.exports = async (req, res, next) => {
   try {
-    const data = await getFilteredTherapist(req.body);
+    const validData = await base('therapist').select({
+      filterByFormula: 'is_valid',
+      sort: [{ field: 'fullName', direction: 'asc' }],
+    }).all();
+    const data = await getFilteredTherapist(req.body, validData);
     res.send({ statusCode: 200, data });
   } catch (err) {
     next(err);
--- server/controllers/utils/filerFunction.js
+++ server/controllers/utils/filerFunction.js
@@ -1,12 +1,6 @@
-const base = require('../../database/config/airtableConnection');
-
 exports.getFilteredTherapist = async ({
   city, types, remote, priceRange,
-}) => {
-  const validData = await base('therapist').select({
-    filterByFormula: 'is_valid',
-    sort: [{ field: 'fullName', direction: 'asc' }],
-  }).all();
+}, validData) => {
   // eslint-disable-next-line max-len
   const filteredData = validData.filter((e) => (types ? e.fields.types === types : true) && (city ? e.fields.city === city : true) && (remote ? e.fields.remote === remote : true) && (priceRange ? e.fields.priceRange === priceRange : true));
   return filteredData;
