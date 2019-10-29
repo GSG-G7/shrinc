7ce741a4428076aab2e19daf2100a38955c662bd
hashemt <hashemta77@gmail.com>
2019-10-29 16:00:45 +0200

    make profile route Relates #56

--- server/controllers/routes/index.js
+++ server/controllers/routes/index.js
@@ -1,2 +1,3 @@
 exports.intialResult = require('./intialResult');
 exports.filter = require('./filter');
+exports.profile = require('./profile');
--- server/router/index.js
+++ server/router/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
-const { clientError, intialResult, filter } = require('../controllers');
+const {
+  clientError, intialResult, filter, profile,
+} = require('../controllers');
 
 const router = express.Router();
 
 router.get('/intial', intialResult);
 router.get('/filter', filter);
+router.get('/profile/:id', profile);
 router.use(clientError);
 
 module.exports = router;
