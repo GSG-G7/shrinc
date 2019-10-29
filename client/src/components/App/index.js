02f8be3bdb6103a849b8c11ee76f8a3ec8ee2fb5
hashemt <hashemta77@gmail.com>
2019-10-30 00:37:58 +0200

    clear app file

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 
-import './style.css';
 import 'antd/dist/antd.css';
-import Signup from '../pages/Signup';
+import './style.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Signup />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
