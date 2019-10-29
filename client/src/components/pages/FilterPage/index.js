d53a29b9d38de7bba3cfdfab404909376ddfca59
fadeomar <fadeomar2015@gmail.com>
2019-10-29 15:36:24 +0200

    delete commented lines

--- client/src/components/pages/FilterPage/index.js
+++ client/src/components/pages/FilterPage/index.js
@@ -5,20 +5,17 @@ import Filter from '../../common/Filter';
 import usersData from '../../common/Card/staticDataTesting';
 import './style.css';
 
-console.log(usersData);
 class FilterPage extends Component {
   state = {
-    data: [1, 2],
+    data: [],
   };
 
   componentDidMount = () => {
-    console.log(11111111111);
     this.setState({ data: usersData });
   };
 
   render() {
     const { data } = this.state;
-    console.log(2222, data);
     return (
       <div>
         <Filter />
