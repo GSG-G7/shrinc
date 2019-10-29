13dcbfeddd0f9c1a06d9f89a64e18fd113b2a331
asmaa <asmathabet2009@hotmail.com>
2019-10-29 13:50:15 +0200

    try to add radio group in step bar relates #62

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-
+import StepsBar from '../common/StepsBar';
 import 'antd/dist/antd.css';
 import './style.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <StepsBar />
+    </div>
+  );
 }
 
 export default App;
--- client/src/components/common/StepsBar/index.js
+++ client/src/components/common/StepsBar/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Steps, Button, message } from 'antd';
+// import {Radio, Input } from 'antd';
 import questions from './questions';
 import './style.css';
 
@@ -8,7 +9,15 @@ const { Step } = Steps;
 class StepsBar extends React.Component {
   state = {
     current: 0,
+    // value:1,
   };
+
+  // onChange = e => {
+  //   // console.log('radio checked', e.target.value);
+  //   this.setState({
+  //     // value: e.target.value,
+  //   });
+  // };
 
   next() {
     this.setState(prevState => ({ current: prevState.current + 1 }));
@@ -20,11 +29,31 @@ class StepsBar extends React.Component {
 
   render() {
     const { current } = this.state;
+    // const radioStyle = {
+    //   display: 'block',
+    //   height: '30px',
+    //   lineHeight: '30px',
+    // };
     return (
       <div>
         <Steps direction="horizontal" current={current}>
           {questions.map(item => (
             <Step direction="horizontal" key={item.title} title={item.title} />
+            //     <Radio.Group onChange={this.onChange} value={this.state.value}>
+            //     <Radio style={radioStyle} value={1}>
+            //       Option A
+            //     </Radio>
+            //     <Radio style={radioStyle} value={2}>
+            //       Option B
+            //     </Radio>
+            //     <Radio style={radioStyle} value={3}>
+            //       Option C
+            //     </Radio>
+            //     <Radio style={radioStyle} value={4}>
+            //       More...
+            //       {this.state.value === 4 ? <Input style={{ width: 100, marginLeft: 10 }} /> : null}
+            //    </Radio>
+            // </Radio.Group>
           ))}
         </Steps>
         <div className="steps-content">{questions[current].content}</div>
