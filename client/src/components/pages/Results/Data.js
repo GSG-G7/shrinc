8b4c45e8736a2e11d315d1ae2fdcf35ae1638ae0
asmaa <asmathabet2009@hotmail.com>
2019-10-29 13:57:20 +0200

    fix pre-commit problems relates #41

--- client/src/components/pages/Results/Data.js
+++
@@ -1,8 +0,0 @@
-const Data = [
-  {
-    key: 1,
-    type: 'Psychodynamic Psychotherapy',
-  },
-];
-
-export default Data;
--- client/src/components/pages/Results/index.js
+++ client/src/components/pages/Results/index.js
@@ -3,14 +3,12 @@ import Card from '../../common/Card';
 
 import './style.css';
 
-const ResultsPage = Data => {
+const ResultsPage = data => {
   return (
     <div className="Results">
       <div className="Results__TherapyType">
         <h3 className="Results__TherapyType__title">Therapy Type</h3>
-        {Data.map(dataType => (
-          <h4 className="Results__TherapyType__name">{dataType.type}</h4>
-        ))}
+        <h4 className="Results__TherapyType__name">{data}</h4>
       </div>
       <div className="Results__TherapistsNames">
         <h3 className="Results__TherapistsNames_suggest">
