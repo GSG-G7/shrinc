0dbabf50f92f9233aedb20faab5fbe2374032f8b
asmaa <asmathabet2009@hotmail.com>
2019-10-29 13:30:54 +0200

    disable result page relates #41

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import Results from '../pages/Results';
+
 import 'antd/dist/antd.css';
 import './style.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Results />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
