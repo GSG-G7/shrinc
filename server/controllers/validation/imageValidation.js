585e040e0e0832e44cbcaead37574570b7cbbd67
hashemt <hashemta77@gmail.com>
2019-10-30 00:31:39 +0200

    edit image validation schema Relates #59

--- server/controllers/validation/imageValidation.js
+++ server/controllers/validation/imageValidation.js
@@ -1,7 +1,7 @@
 const yup = require('yup');
 
 module.exports = yup.object({
-  image: yup.mixed().test('fileSize', 'File Size is too large', (value) => value.size <= 10000)
-    .test('fileType', 'Unsupported File Format', (value) => ['image/jpg', 'image/jpeg', 'image/gif',
-      'image/png'].includes(value.type)).required(),
+  image: yup.mixed().test('fileSize', 'File Size is too large', (image) => (image.size / 1024 / 1024) < 500)
+    .test('fileType', 'Unsupported File Format', (image) => ['image/jpg', 'image/jpeg', 'image/gif',
+      'image/png'].includes(image.type)),
 });
