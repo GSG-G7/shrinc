54bcf5d473fb6ea3203b0302e3111478f242e8e8
fadeomar <fadeomar2015@gmail.com>
2019-10-29 09:48:42 +0200

    reset the app file

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -2,14 +2,9 @@ import React from 'react';
 
 import 'antd/dist/antd.css';
 import './style.css';
-import Card from '../common/Card';
 
 function App() {
-  return (
-    <div className="App">
-      <Card />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
