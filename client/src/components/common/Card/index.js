42b9f81405c3378c586478b9298ea2db7dd051ce
fadeomar <fadeomar2015@gmail.com>
2019-10-29 15:36:02 +0200

    add propstype for data from props to card component

--- client/src/components/common/Card/index.js
+++ client/src/components/common/Card/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Collapse } from 'antd';
+import PropTypes from 'prop-types';
 
 import './style.css';
 import CardContent from './cardContent';
@@ -9,27 +10,36 @@ const { Panel } = Collapse;
 const genExtra = price => (
   <span className="card__price">{price} $/Session</span>
 );
-const Card = data => {
-  return (
-    <Collapse accordion>
-      {console.log(data)}
-      {data.map(({ avatar, name, approach, type, priceRange }) => (
-        <Panel
-          header={name}
-          key={name}
-          extra={genExtra(priceRange)}
-          className="card__header"
-        >
-          <CardContent
-            avatar={avatar}
-            name={name}
-            approach={approach}
-            type={type}
-          />
-        </Panel>
-      ))}
-    </Collapse>
-  );
+const Card = ({ data }) => (
+  <Collapse accordion>
+    {data.map(({ avatar, name, approach, type, priceRange }) => (
+      <Panel
+        header={name}
+        key={name}
+        extra={genExtra(priceRange)}
+        className="card__header"
+      >
+        <CardContent
+          avatar={avatar}
+          name={name}
+          approach={approach}
+          type={type}
+        />
+      </Panel>
+    ))}
+  </Collapse>
+);
+
+Card.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      approach: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      priceRange: PropTypes.number.isRequired,
+    }).isRequired
+  ).isRequired,
 };
 
 export default Card;
