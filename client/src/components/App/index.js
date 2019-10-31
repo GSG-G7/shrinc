5decba87f035b535321f0fb5614a9b54b5b58876
asmaa <asmathabet2009@hotmail.com>
2019-10-31 09:23:46 +0200

    add some edits relates #89

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,55 +1,49 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Header from '../common/Header';
-import Home from '../pages/Home';
-import Signup from '../pages/Signup';
-import About from '../pages/About';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import TherapyType from '../pages/TherapyType';
 import Glossary from '../pages/Glossary';
 import Profile from '../pages/Profile';
-import TherapyType from '../pages/TherapyType';
+import Header from '../common/Header';
+import Signup from '../pages/Signup';
+import About from '../pages/About';
+import Home from '../pages/Home';
 
 import 'antd/dist/antd.css';
 import './style.css';
 
-class App extends Component {
-  state = {
-    // eslint-disable-next-line react/no-unused-state
-    auth: null,
-  };
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <main className="container">
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
 
-  render() {
-    return (
-      <>
-        <Header />
-        <main className="container">
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
+          <Route exact path="/signup">
+            <Signup />
+          </Route>
 
-            <Route exact path="/signup">
-              <Signup />
-            </Route>
+          <Route exact path="/About">
+            <About />
+          </Route>
 
-            <Route exact path="/About">
-              <About />
-            </Route>
+          <Route exact path="/glossary">
+            <Glossary />
+          </Route>
 
-            <Route exact path="/glossary">
-              <Glossary />
-            </Route>
+          <Route exact path="/profile/:id">
+            {props => <Profile {...props} />}
+          </Route>
 
-            <Route exact path="/profile/:name">
-              {props => <Profile {...props} />}
-            </Route>
-
-            <Route exact path="/types">
-              <TherapyType />
-            </Route>
-          </Switch>
-        </main>
-      </>
-    );
-  }
+          <Route exact path="/types">
+            <TherapyType />
+          </Route>
+        </Switch>
+      </main>
+    </BrowserRouter>
+  );
 }
 export default App;
