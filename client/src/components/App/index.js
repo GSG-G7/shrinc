1219a526a37f083733bf10f7b37434e4d2061c4f
asmaa <asmathabet2009@hotmail.com>
2019-10-31 09:03:14 +0200

    add routes to the app file relates #89

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Header from '../common/Header';
+import Home from '../pages/Home';
+import Signup from '../pages/Signup';
+import About from '../pages/About';
+import Glossary from '../pages/Glossary';
+import Profile from '../pages/Profile';
+import TherapyType from '../pages/TherapyType';
 
 import 'antd/dist/antd.css';
 import './style.css';
 
-function App() {
-  return <div className="App"></div>;
-}
+class App extends Component {
+  state = {
+    // eslint-disable-next-line react/no-unused-state
+    auth: null,
+  };
 
+  render() {
+    return (
+      <>
+        <Header />
+        <main className="container">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+
+            <Route exact path="/signup">
+              <Signup />
+            </Route>
+
+            <Route exact path="/About">
+              <About />
+            </Route>
+
+            <Route exact path="/glossary">
+              <Glossary />
+            </Route>
+
+            <Route exact path="/profile/:name">
+              {props => <Profile {...props} />}
+            </Route>
+
+            <Route exact path="/types">
+              <TherapyType />
+            </Route>
+          </Switch>
+        </main>
+      </>
+    );
+  }
+}
 export default App;
