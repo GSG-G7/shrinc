31efae4207b5d57dd9d0dac204531b88b1b95321
fadeomar <fadeomar2015@gmail.com>
2019-11-01 12:25:19 +0200

    q component that return radio group

---
+++ client/src/components/common/Questionary/Question.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Radio } from 'antd';
+
+import './style.css';
+
+const Question = props => {
+  const { onChange, options, id, value, keyValue } = props;
+  return (
+    <>
+      {options[id].question && <h1>{options[id].question}</h1>}
+
+      <Radio.Group key={keyValue} onChange={onChange} value={value}>
+        {options[id].answers.map((e, index) => (
+          <Radio value={`${index}`} id={id} key={`${e}`}>
+            {e}
+          </Radio>
+        ))}
+      </Radio.Group>
+    </>
+  );
+};
+
+Question.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+  keyValue: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  options: PropTypes.objectOf(PropTypes.object).isRequired,
+};
+export default Question;
