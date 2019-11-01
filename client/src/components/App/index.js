1ec78e84dc518fb25c1cc8695ab510ad6dbc65d1
fadeomar <fadeomar2015@gmail.com>
2019-11-01 20:07:22 +0200

    separade the rates data from the function

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 import 'antd/dist/antd.css';
 import './style.css';
+import QuestionPage from '../pages/QuestionaryPage';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <QuestionPage />
+    </div>
+  );
 }
 
 export default App;
