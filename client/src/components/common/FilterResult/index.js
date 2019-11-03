c2d6e6d1400ccf06c1a8ed18d26105e35865fcfe
fadeomar <fadeomar2015@gmail.com>
2019-11-03 12:44:19 +0200

    add introduction for the component

--- client/src/components/common/FilterResult/index.js
+++ client/src/components/common/FilterResult/index.js
@@ -1,1 +1,15 @@
+import React from 'react';
 import './style.css';
+
+const FilterResult = () => {
+  return (
+    <div>
+      <h3>
+        According to your answers to the questions your doctors have assessed
+        and points You need the most points doctor
+      </h3>
+    </div>
+  );
+};
+
+export default FilterResult;
