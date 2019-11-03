c7d0e0ec327593e3f9e790e9bb001aeed9e32c81
fadeomar <fadeomar2015@gmail.com>
2019-11-03 12:52:03 +0200

    change the Questionary page from class to function

--- client/src/components/pages/QuestionaryPage/index.js
+++ client/src/components/pages/QuestionaryPage/index.js
@@ -1,19 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Questionary from '../../common/Questionary';
 import './style.css';
 
-class QuestionaryPage extends Component {
-  state = {};
-
-  render() {
-    return (
-      <div className="questionary__container">
-        <h1 className="questionary__title">Questionnaire description</h1>
-        <Questionary />;
-      </div>
-    );
-  }
-}
+const QuestionaryPage = () => (
+  <div className="questionary__container">
+    <h1 className="questionary__title">Questionnaire description</h1>
+    <Questionary />;
+  </div>
+);
 
 export default QuestionaryPage;
