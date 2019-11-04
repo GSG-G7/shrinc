d9c0a93ebb68d783171887b8d843e95e8aba6cf5
hashemt <hashemta77@gmail.com>
2019-11-04 13:56:53 +0200

    create map component Relates #117

--- client/src/components/common/Location/index.js
+++ client/src/components/common/Location/index.js
@@ -1,36 +1,51 @@
 import React from 'react';
-import propTypes from 'prop-types';
-import { Icon } from 'antd';
+import L from 'leaflet';
+import * as geocoding from 'esri-leaflet-geocoder';
 
 import './style.css';
 
-const Location = ({ locationInfo: { address, postalCode, map } }) => {
-  return (
-    <section>
-      <div className="location_contact_container">
-        <h2>
-          <Icon type="global" /> Location
-        </h2>
-        <div className="location_contact_info">
-          <div className="location_info">
-            <h5 className="location_titles">Address</h5>
-            <p>{address}</p>
-          </div>
-          <div className="location_info">
-            <h5 className="location_titles">Postal Code</h5>
-            <p>{postalCode}</p>
-          </div>
-        </div>
-      </div>
-      <img src={map} alt="therapist location" className="location_img_map" />
-    </section>
-  );
-};
-export default Location;
-Location.propTypes = {
-  locationInfo: propTypes.shape({
-    address: propTypes.string.isRequired,
-    postalCode: propTypes.string.isRequired,
-    map: propTypes.string.isRequired,
-  }).isRequired,
-};
+class Map extends React.Component {
+  state = {
+    // eslint-disable-next-line react/no-unused-state
+    city: '',
+    theMarker: {},
+  };
+
+  componentDidMount() {
+    this.map = L.map('map').setView([51.505, -0.09], 13);
+    L.tileLayer(
+      'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}',
+      {
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken:
+          'pk.eyJ1Ijoic2hyaW5jIiwiYSI6ImNrMml6c3pyYjFlbmwzY3A1bGF1djE4cDkifQ.vWHQYdsTFHFec905OCTzzw',
+      }
+    ).addTo(this.map);
+    this.map.on('click', this.onMapClick);
+  }
+
+  onMapClick = ({ latlng }) => {
+    const { theMarker } = this.state;
+    this.map.removeLayer(theMarker);
+    this.setState({
+      theMarker: L.marker([latlng.lat, latlng.lng]).addTo(this.map),
+    });
+    const geo = geocoding.geocodeService();
+    geo
+      .reverse()
+      .latlng(latlng)
+      .run((error, result) => {
+        // eslint-disable-next-line react/no-unused-state
+        if (!error) this.setState({ city: result.address.City });
+      });
+  };
+
+  render() {
+    return <div id="map"></div>;
+  }
+}
+
+export default Map;
