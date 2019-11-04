6dbd5f106fed9af50c19322d41f3fda131b24535
hashemt <hashemta77@gmail.com>
2019-11-04 14:37:27 +0200

    create cities route Relates #122

--- server/router/index.js
+++ server/router/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const {
-  clientError, intialResult, filter, profile, signup,
+  clientError, intialResult, filter, profile, signup, cities,
 } = require('../controllers');
 
 const router = express.Router();
@@ -10,6 +10,7 @@ router.post('/signup', signup);
 router.get('/intial', intialResult);
 router.post('/filter', filter);
 router.get('/profile/:id', profile);
+router.get('/cities', cities);
 router.use(clientError);
 
 module.exports = router;
