ab428b04b34ce69ba2618dd2cda3f1d61d8ee57b
fadeomar <fadeomar2015@gmail.com>
2019-11-06 18:56:57 +0200

    change the place of result route

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -22,19 +22,18 @@ export default () => {
   return (
     <BrowserRouter>
       <Header />
-
       <main className="container">
         <Helmet>
           <title>Shrinc</title>
         </Helmet>
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/result" component={Results} />
           <Route path="/signup" component={Signup} />
           <Route path="/About" component={About} />
           <Route path="/glossary" component={Glossary} />
           <Route exact path="/profile/:id" component={Profile} />
           <Route path="/types" component={TherapyType} />
+          <Route exact path="/result" component={Results} />
           <Route path="/filter" component={Filter} />
           <Route path="/questionnaire" component={Qustionnarie} />
           <Route path="*" component={NotFound} />
