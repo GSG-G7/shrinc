07bcf8dab4f3d1cbe05c723a7bec1417ceb994f4
asmaa <asmathabet2009@hotmail.com>
2019-11-11 11:24:26 +0200

    remove semicolon from Questionnary page relates #150

--- client/src/components/pages/QuestionaryPage/index.js
+++ client/src/components/pages/QuestionaryPage/index.js
@@ -6,7 +6,7 @@ import './style.css';
 const QuestionaryPage = () => (
   <div className="questionary__container">
     <h1 className="questionary__title">Questionnaire description</h1>
-    <Questionary />;
+    <Questionary />
   </div>
 );
 
