0fde243c8b0bafc23e6ad1c96d5ecf58ba91b428
asmaa <asmathabet2009@hotmail.com>
2019-11-19 11:01:24 +0200

    add therpay types to static data relates #180

--- client/src/components/pages/TherapyType/staticData.js
+++ client/src/components/pages/TherapyType/staticData.js
@@ -1,159 +1,38 @@
 export default [
   {
     key: 1,
-    title: 'Psychodynamic Psychotherapy',
-    text: `psychodynamic psychotherapy draws on theories and practices of analytical psychology
-  and psychoanalysis. It is a therapeutic process which helps patients understand and
-  resolve their problems by increasing awareness of their inner world and its influence
-  over relationships both past and present. It differs from most other therapies in aiming
-  for deep seated change in personality and emotional development.
-  Psychoanalytic and psychodynamic psychotherapy aim to help people with serious
-  psychological disorders to understand and change complex, deep-seated and often
-  unconsciously based emotional and relationship problems thereby reducing symptoms
-  and alleviating distress. However, their role is not limited only to those with mental
-  health problems. Many people who experience a loss of meaning in their lives or who
-  are seeking a greater sense of fulfilment may be helped by psychoanalytic or
-  psychodynamic psychotherapy.
-  The relationship with the therapist is a crucial element in the therapy. The therapist
-  offers a confidential and private setting which facilitates a process where unconscious
-  patterns of the patient's inner world become reflected in the patient's relationship with
-  the therapist (transference). This process helps patients gradually to identify these
-  patterns and, in becoming conscious of them, to develop the capacity to understand
-  and change them.`,
+    title: 'Cognitive Behavioural Therapy',
+    text: `Problem solving to manage your difficulties
+    Cognitive behavioural therapy is a type of therapy that puts the emphasis on the therapist and the client working together to address present difficulties using strategies and rational thinking. A CBT therapist is trained in offering clients tools to reduce the impact of mental health symptoms and try active strategies such as changes in behaviour or guided problem solving, to reduce depression and anxiety. It does not address interpersonal issues in relationships and does not aim at changing the unconscious factors influencing a client’s distress. It requires a commitment from the client to participate actively by doing exercises and putting advice and strategies into practice. A course of CBT therapy typically lasts between 1 and 6 months.`,
   },
   {
     key: 2,
-    title: 'Cognitive Behavioural Therapy',
-    text: `psychodynamic psychotherapy draws on theories and practices of analytical psychology
-    and psychoanalysis. It is a therapeutic process which helps patients understand and
-    resolve their problems by increasing awareness of their inner world and its influence
-    over relationships both past and present. It differs from most other therapies in aiming
-    for deep seated change in personality and emotional development.
-    Psychoanalytic and psychodynamic psychotherapy aim to help people with serious
-    psychological disorders to understand and change complex, deep-seated and often
-    unconsciously based emotional and relationship problems thereby reducing symptoms
-    and alleviating distress. However, their role is not limited only to those with mental
-    health problems. Many people who experience a loss of meaning in their lives or who
-    are seeking a greater sense of fulfilment may be helped by psychoanalytic or
-    psychodynamic psychotherapy.
-    
-    The relationship with the therapist is a crucial element in the therapy. The therapist
-    offers a confidential and private setting which facilitates a process where unconscious
-    patterns of the patient's inner world become reflected in the patient's relationship with
-    the therapist (transference). This process helps patients gradually to identify these
-    patterns and, in becoming conscious of them, to develop the capacity to understand
-    and change them.`,
+    title: 'Psychodynamic Psychotherapy',
+    text: `Discovering and transforming uncosncious patterns in your psyche
+    Psychodynamic pyschitherapy aims at undertsanding and transforming uncosncious or involuntary deep seated patterns in a client’s thinking and relationships, and in his or her general attitude towards life’s challenges. One may want to understake this kind of treatment when there is a sense of failure in one or more aspects of one’s relationships or dissatisfaction with an important area of one’s life withut knowing exactly how to change it. A psychodynamic psychiotherapy will guide the client in undersatnding some patterns in relating to themselves or others that the client has without knowing and that can be a source of great frustration and pain. The therapist would guide the client in becoming aware of their origin and the way these patterns are active in their present relationships, including their present relationship with their therapist. This therapy requires a commitment to understadn and trasnofrm deeper aspects of one’s personality and a sense of curiosity towards one’s own psychological functioning. Sessions can be once or twice a week. A course of psychodynamic psychotherapy typically lasts between 1 and 3 years, during which a sgnificant change in awareness and experience is achieved.`,
   },
   {
     key: 3,
     title: 'Humanistic Psychotherapy',
-    text: `psychodynamic psychotherapy draws on theories and practices of analytical psychology
-    and psychoanalysis. It is a therapeutic process which helps patients understand and
-    resolve their problems by increasing awareness of their inner world and its influence
-    over relationships both past and present. It differs from most other therapies in aiming
-    for deep seated change in personality and emotional development.
-    Psychoanalytic and psychodynamic psychotherapy aim to help people with serious
-    psychological disorders to understand and change complex, deep-seated and often
-    unconsciously based emotional and relationship problems thereby reducing symptoms
-    and alleviating distress. However, their role is not limited only to those with mental
-    health problems. Many people who experience a loss of meaning in their lives or who
-    are seeking a greater sense of fulfilment may be helped by psychoanalytic or
-    psychodynamic psychotherapy.
-    The relationship with the therapist is a crucial element in the therapy. The therapist
-    offers a confidential and private setting which facilitates a process where unconscious
-    patterns of the patient's inner world become reflected in the patient's relationship with
-    the therapist (transference). This process helps patients gradually to identify these
-    patterns and, in becoming conscious of them, to develop the capacity to understand
-    and change them.`,
+    text: `Seeing every individual as a whole, offering unconditional support
+    The main concept behind humanistic approaches is unconditional, positive regard for the client as an individual with his or her own potetnial for self realisation. The therapeutic relatiosnship provides a supportive environment for each individual to become more aware of themselves and able to make informed and helpful decisions in their life and relationships. A humanistic psychotherapy will provide warmth and acceptance of the client’s predicament, to help the client in turn accept themselves as a whole, and to reach an increased sense of integrity and autonomy. Gestalt and Client Centered therapy are two main modailties of humanistic psychotherapy (please se the glossary for more detail). They typical length of huamanistic psychotherapy varies between six months and several years.`,
   },
   {
     key: 4,
     title: 'Integrative Psychotherapy',
-    text: `psychodynamic psychotherapy draws on theories and practices of analytical psychology
-    and psychoanalysis. It is a therapeutic process which helps patients understand and
-    resolve their problems by increasing awareness of their inner world and its influence
-    over relationships both past and present. It differs from most other therapies in aiming
-    for deep seated change in personality and emotional development.
-    Psychoanalytic and psychodynamic psychotherapy aim to help people with serious
-    psychological disorders to understand and change complex, deep-seated and often
-    
-    unconsciously based emotional and relationship problems thereby reducing symptoms
-    and alleviating distress. However, their role is not limited only to those with mental
-    health problems. Many people who experience a loss of meaning in their lives or who
-    are seeking a greater sense of fulfilment may be helped by psychoanalytic or
-    psychodynamic psychotherapy.
-    The relationship with the therapist is a crucial element in the therapy. The therapist
-    offers a confidential and private setting which facilitates a process where unconscious
-    patterns of the patient's inner world become reflected in the patient's relationship with
-    the therapist (transference). This process helps patients gradually to identify these
-    patterns and, in becoming conscious of them, to develop the capacity to understand
-    and change them.`,
+    text: `An ecletic approach to finding more awareness and happiness
+    Integrative psychotherapy can be described as an apporach where the therapist uses a variety of techniques, taken from a variety of theories including psychodynamic, interpersonal, cognitive, behavioural and body psychotherapy to offer a flexible approach to a client’s specific needs. The overall aim would be to increase the client’s self awareness by integrating unwanted parts of the self that are casuing distress and internal or interpersonal conflict. While the theroretical underpinnings of the ways in which a therapist will work are less rigorous than in psychodynamic psychotherapy and more eclectic, there will be an emphasis on flexibility and adaptability of the therapy to the specific needs of the client. A course of integrative psychotherapy typically lasts between six months and a few years.`,
   },
   {
     key: 5,
-    title: 'Short Term Psychodynamic Psychotherapy',
-    text: `psychodynamic psychotherapy draws on theories and practices of analytical psychology
-    and psychoanalysis. It is a therapeutic process which helps patients understand and
-    resolve their problems by increasing awareness of their inner world and its influence
-    over relationships both past and present. It differs from most other therapies in aiming
-    for deep seated change in personality and emotional development.
-    Psychoanalytic and psychodynamic psychotherapy aim to help people with serious
-    psychological disorders to understand and change complex, deep-seated and often
-    unconsciously based emotional and relationship problems thereby reducing symptoms
-    and alleviating distress. However, their role is not limited only to those with mental
-    health problems. Many people who experience a loss of meaning in their lives or who
-    are seeking a greater sense of fulfilment may be helped by psychoanalytic or
-    psychodynamic psychotherapy.
-    The relationship with the therapist is a crucial element in the therapy. The therapist
-    offers a confidential and private setting which facilitates a process where unconscious
-    patterns of the patient's inner world become reflected in the patient's relationship with
-    the therapist (transference). This process helps patients gradually to identify these
-    patterns and, in becoming conscious of them, to develop the capacity to understand
-    and change them.`,
+    title: 'Counselling',
+    text: `A safe space to be listened and overcome a difficult time
+    Counselling, unlike psychotherapy, does not aim at shifting deep seated psychological problems, and instead provides a safe space where a client can be listened sympathertically, and given advice and coping strategies to manage emotional distress. Counselling can be a good choice when a client who otherwise is happy with their identity and relationships is undergoing a difficult time in relation to a painful event such as a bereavement, a divorce, or any other life event that is particularily challenging and overwhelming. Counselling can consist even in one or two sessions, or can last longer, and become more akin to psychotherapy, if the counsellor and the client deepen their relationsip and the client becomes interested in achieving a substantial psychological change. Counsellors’ work is also influenced by three main theoretical currents: psychodynamic, integrative or humanistic.`,
   },
   {
     key: 6,
-    title: 'Counselling',
-    text: `psychodynamic psychotherapy draws on theories and practices of analytical psychology
-    and psychoanalysis. It is a therapeutic process which helps patients understand and
-    resolve their problems by increasing awareness of their inner world and its influence
-    over relationships both past and present. It differs from most other therapies in aiming
-    for deep seated change in personality and emotional development.
-    Psychoanalytic and psychodynamic psychotherapy aim to help people with serious
-    psychological disorders to understand and change complex, deep-seated and often
-    unconsciously based emotional and relationship problems thereby reducing symptoms
-    and alleviating distress. However, their role is not limited only to those with mental
-    health problems. Many people who experience a loss of meaning in their lives or who
-    are seeking a greater sense of fulfilment may be helped by psychoanalytic or
-    psychodynamic psychotherapy.
-    The relationship with the therapist is a crucial element in the therapy. The therapist
-    offers a confidential and private setting which facilitates a process where unconscious
-    patterns of the patient's inner world become reflected in the patient's relationship with
-    the therapist (transference). This process helps patients gradually to identify these
-    patterns and, in becoming conscious of them, to develop the capacity to understand
-    and change them.`,
-  },
-  {
-    key: 7,
-    title: 'EMDR',
-    text: `psychodynamic psychotherapy draws on theories and practices of analytical psychology
-    and psychoanalysis. It is a therapeutic process which helps patients understand and
-    resolve their problems by increasing awareness of their inner world and its influence
-    over relationships both past and present. It differs from most other therapies in aiming
-    for deep seated change in personality and emotional development.
-    Psychoanalytic and psychodynamic psychotherapy aim to help people with serious
-    psychological disorders to understand and change complex, deep-seated and often
-    unconsciously based emotional and relationship problems thereby reducing symptoms
-    and alleviating distress. However, their role is not limited only to those with mental
-    health problems. Many people who experience a loss of meaning in their lives or who
-    are seeking a greater sense of fulfilment may be helped by psychoanalytic or
-    psychodynamic psychotherapy.
-    The relationship with the therapist is a crucial element in the therapy. The therapist
-    offers a confidential and private setting which facilitates a process where unconscious
-    patterns of the patient's inner world become reflected in the patient's relationship with
-    the therapist (transference). This process helps patients gradually to identify these
-    
-    patterns and, in becoming conscious of them, to develop the capacity to understand
-    and change them.`,
+    title: 'Brief psychodynamic psychotherapy',
+    text: `Focussed, intensive work on unconscious patterns in your relationships
+    This kind of therapy is similar in aim and technique to traditional psychodynamic psychothreapy however it is more limited in its scope and length, and it is suitable for those who wish to work with depth and rigour on one specific unconsicuos aspects of their behaviour and relationship without having to commit to years of therapy. The client is required to commit to working in a focussed manner, to achieve a significant change in their awareness in a relatively shorter time. The average length of brief psychodynamic psychotherapies varies between 3 and 6 months.`,
   },
 ];
