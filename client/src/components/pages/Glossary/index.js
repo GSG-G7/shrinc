69e0c0824cd33d6cf615feea048f3247ea5e0cfc
fadeomar <fadeomar2015@gmail.com>
2019-11-19 13:13:46 +0200

    refactor the glossary page

--- client/src/components/pages/Glossary/index.js
+++ client/src/components/pages/Glossary/index.js
@@ -10,22 +10,57 @@ const { Search } = Input;
 class Glossary extends Component {
   state = {
     searchValue: '',
-    items: [],
+    items: glossaryData,
   };
 
-  onChange = ({ target: { value } }) => {
-    const searchValue = value.toLowerCase();
-    const items = glossaryData.filter(item =>
+  componentDidUpdate(prevProps, prevState) {
+    const { searchValue } = this.state;
+    if (prevState.searchValue !== searchValue) this.filterItems();
+  }
+
+  filterItems = () => {
+    const { searchValue } = this.state;
+
+    let items = glossaryData.filter(item =>
       Object.keys(item)[0]
         .toLowerCase()
         .includes(searchValue)
     );
-    this.setState({ items, searchValue });
+
+    if (!items.length) items = glossaryData;
+
+    this.setState({
+      items,
+    });
+  };
+
+  handleChange = ({ target: { value } }) => {
+    const searchValue = value.toLowerCase();
+    this.setState({ searchValue });
+  };
+
+  renderItems = () => {
+    const { items, searchValue } = this.state;
+
+    if (searchValue && !items.length)
+      return <h3>your search does not match any types</h3>;
+
+    return items.map(item => {
+      const term = Object.keys(item);
+      const meaning = Object.values(item);
+
+      return (
+        <Fragment key={term}>
+          <h3>{term}</h3>
+          <div>
+            <p className="glossary_content">{meaning}</p>
+          </div>
+        </Fragment>
+      );
+    });
   };
 
   render() {
-    const { items: currentItems, searchValue } = this.state;
-    const items = currentItems.length ? currentItems : glossaryData;
     return (
       <section className="glossary_container">
         <Helmet>
@@ -34,25 +69,10 @@ class Glossary extends Component {
         <h1>Glossary Page</h1>
         <Search
           placeholder="input search text"
-          onChange={this.onChange}
+          onChange={this.handleChange}
           style={{ width: 200 }}
         />
-        {searchValue && !currentItems.length ? (
-          <h3>your search does not match any types</h3>
-        ) : (
-          items.map(item => {
-            const term = Object.keys(item);
-            const meaning = Object.values(item);
-            return (
-              <Fragment key={term}>
-                <h3>{term}</h3>
-                <div>
-                  <p className="glossary_content">{meaning}</p>
-                </div>
-              </Fragment>
-            );
-          })
-        )}
+        {this.renderItems()}
       </section>
     );
   }
