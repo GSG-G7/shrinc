d80977398c22e4e95fcbe6d44bc039caad557705
MohammadAlhalaq <m-a-s-h@outlook.com>
2019-11-19 11:37:32 +0200

    edit error response Relates #201

--- server/app.js
+++ server/app.js
@@ -27,7 +27,15 @@ app.get('*', (req, res) => {
 app.use((err, req, res, next) => {
   console.log('Path', req.path);
   console.log('Error from 500', err);
-  res.status(500).send({ message: 'Internal Server Error', statusCode: 500 });
+  switch (err.statusCode) {
+    case 404:
+      res.status(404).send(err);
+      break;
+
+    default:
+      res.status(500).send({ message: 'Internal Server Error', statusCode: 500 });
+      break;
+  }
 });
 
 module.exports = app;
