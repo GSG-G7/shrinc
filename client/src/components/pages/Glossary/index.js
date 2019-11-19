b80a92840acdc5fb8ce3bf02e49f8902d20ab390
fadeomar <fadeomar2015@gmail.com>
2019-11-19 02:10:20 +0200

    add the search bar in the glossary

--- client/src/components/pages/Glossary/index.js
+++ client/src/components/pages/Glossary/index.js
@@ -1,26 +1,59 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import { Helmet } from 'react-helmet';
+import { Input } from 'antd';
 
 import glossaryData from './glossaryData.json';
 import './style.css';
 
-export default () => {
-  return (
-    <section className="glossary_container">
-      <Helmet>
-        <title>Glossary</title>
-      </Helmet>
-      <h1>Glossary Page</h1>
-      {glossaryData.map(item => {
-        return (
-          <Fragment key={Object.keys(item)}>
-            <h3>{Object.keys(item)}</h3>
-            <div>
-              <p className="glossary_content">{Object.values(item)}</p>
-            </div>
-          </Fragment>
-        );
-      })}
-    </section>
-  );
-};
+const { Search } = Input;
+
+class Glossary extends Component {
+  state = {
+    searchValue: '',
+    items: [],
+  };
+
+  onChange = v => {
+    const searchValue = v.target.value.toUpperCase();
+    const items = glossaryData.filter(item =>
+      Object.keys(item)[0].includes(searchValue)
+    );
+    this.setState(() => ({ items, searchValue }));
+  };
+
+  render() {
+    const { items: currentItems, searchValue } = this.state;
+    const items = currentItems.length ? currentItems : glossaryData;
+    return (
+      <section className="glossary_container">
+        <Helmet>
+          <title>Glossary</title>
+        </Helmet>
+        <h1>Glossary Page</h1>
+        <Search
+          placeholder="input search text"
+          onChange={this.onChange}
+          style={{ width: 200 }}
+        />
+        {searchValue && !currentItems.length ? (
+          <h3>your search does not match any types</h3>
+        ) : (
+          items.map(item => {
+            const term = Object.keys(item);
+            const meaning = Object.values(item);
+            return (
+              <Fragment key={term}>
+                <h3>{term}</h3>
+                <div>
+                  <p className="glossary_content">{meaning}</p>
+                </div>
+              </Fragment>
+            );
+          })
+        )}
+      </section>
+    );
+  }
+}
+
+export default Glossary;
