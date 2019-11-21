cd54cd6e4766aa97737c208eab75192fda17de70
asmaa <asmathabet2009@hotmail.com>
2019-11-21 09:58:57 +0200

    Change text to the attached in About section relates#208

--- client/src/components/pages/About/index.js
+++ client/src/components/pages/About/index.js
@@ -14,54 +14,40 @@ const About = () => {
         <li className="about-page__point">
           Our Mission
           <p className="about-page__content">
-            Shrinc is the new way to access talking therapies, quickly and
-            easily
+            At Shrinc we believe that finding the right psychotherapy for you
+            can life changing. However, finding a good psychotherapist can be
+            daunting. Shrinc is here to help. Shrinc is the new way to access
+            talking therapies, helping you to find the most appropriate therapy
+            and the best therapist in a few easy steps.
           </p>
         </li>
         <li className="about-page__point">
-          Our Vision
+          Finding a therapist has never been easier
           <p className="about-page__content">
-            Shrinc provides users with free access to a complete overview of the
-            main forms of talking therapy, which are presented in a clear and
-            concise manner. Shrinc also contains a database of therapists that
-            allows users to search by therapist type, map-based location, cost,
-            availability, and the option of remote consultation.
+            The Shrinc database allows you to quickly and easily search for
+            therapists according to location, price and specialisation. If you
+            are new to therapy, you can use the Find the right therapy feature
+            which will help to guide you to the most appropriate therapy type.
+            Shrinc also contains a brief overview of each of the main forms of
+            psychotherapy. This then links to the database to allow you to
+            easily search for the relevant therapists in your area. Shrinc also
+            offer remote consultation sessions for those users that are either
+            not local to their chosen therapist, or cannot attend sessions in
+            person. All our therapists are registered with the main UK
+            accrediting bodies to ensure a high standard of training and care.
+            Booking and paying for your session can also be done through Shrinc.
+            with automatic appointment reminders and payment notifications
+            occurring before the session takes place.
           </p>
         </li>
         <li className="about-page__point">
-          Questionnire
+          Your opinion matters
           <p className="about-page__content">
-            Users can also complete a questionnaire to help identify the type of
-            therapy that is most suitable for them. Shrinc will then present the
-            most relevant therapists, allowing the user to apply further filters
-            as described above.
-          </p>
-        </li>
-        <li className="about-page__point">
-          Review system
-          <p className="about-page__content">
-            All therapists are verified for their qualifications and
-            professional accreditation. There is a review system that allows
-            Shrinc customers to leave feedback of their experience. All reviews
-            are moderated, with negative reviews handled discreetly by Shrinc
-            administrators. Therapists with more than 3 negative reviews in any
-            six month period are reviewed by Shrinc administrators and removed
-            from the database if necessary.
-          </p>
-        </li>
-        <li className="about-page__point">
-          Select therapist
-          <p className="about-page__content">
-            Once a user has selected a therapist, users can register with Shrinc
-            for free which allows them to easily book a consultation using a
-            web-based calendar.
-          </p>
-        </li>
-        <li className="about-page__point">
-          Payment
-          <p className="about-page__content">
-            Payment is made through Shrinc using either Paypal or debit card at
-            the rate provided by the therapist.
+            There is a review system that allows you to leave feedback of your
+            experience. All reviews are moderated, with negative reviews handled
+            discreetly by Shrinc administrators. Therapists with more than 3
+            negative reviews in any six month period are reviewed by Shrinc
+            administrators and removed from the database if necessary.
           </p>
         </li>
       </ul>
